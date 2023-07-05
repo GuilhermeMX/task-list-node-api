7448150b286d96febf41e7d5043e58f49056a2ac
Guilherme M. Xavier <guilhermenen15@gmail.com>
2023-07-05 13:37:30 -0300

    update server

--- src/server.js
+++ src/server.js
@@ -12,20 +12,21 @@ const server = http.createServer(async (req, res) => {
 
   // Testando se o regex bate com a url da requisição
   const route = routes.find(route => {
-    const routeParams = req.url.match(route.path);
+    return route.method == method && route.path.test(url); 
+  })
 
+  if (route) {
+    const routeParams = req.url.match(route.path);
     // Declarando as variáveis de forma desestruturada de dentro da variável routeParams
     const { query, ...params } = routeParams.groups
 
     req.params = params
     req.query = query ? extractQueryParams(query) : {}
     
-    return route.method == method && route.path.test(url); 
-  })
-
-  if (route) {
     return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
 })
+
+server.listen(3333);
